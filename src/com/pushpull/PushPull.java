811cc446cffdcda8ac611b0a85b2a880a5032696
mathisankar14 <smathisankar@gmail.com>
2022-10-29 13:45:45 +0530

    mathi task

--- src/com/pushpull/PushPull.java
+++ src/com/pushpull/PushPull.java
@@ -4,18 +4,26 @@ public class PushPull {
 	private void empName() {
 		System.out.println("EmpName: Mathi");
 	}
+
 	private void empID() {
 		System.out.println("EmpID: 123");
 	}
+
 	private void empMail() {
 		System.out.println("EmpMail: mathis@gmail.com");
 	}
+
+	private void empAddress() {
+		System.out.println("EmpAddress: chennai");
+	}
+
 	public static void main(String[] args) {
 		PushPull pp = new PushPull();
 		pp.empName();
 		pp.empID();
 		pp.empMail();
-		
+		pp.empAddress();
+
 	}
 
 }
